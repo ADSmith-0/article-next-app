77e6cca49eb1313b4c32b14b07d2bacfd06e513f
ADSmith-0 <adam00199382818@gmail.com>
2021-08-08 15:42:34 +0100

    update: basic card component finished

---
+++ Types/CardType.tsx
@@ -0,0 +1,8 @@
+export type CardType = {
+    image: string,
+    title: string, 
+    domain: string, 
+    date: Date, 
+    content: string, 
+    tag: 'Sports' | 'Marketing' | 'Environment'
+}
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,22 @@
-export default function Card() {
+import Image from 'next/image';
+import styles from '../styles/Card.module.css';
+import { CardType } from '../Types/CardType';
+export default function Card({ image, title, domain, date, content, tag }:CardType) {
     return (
-        <div>
-            
-        </div>
+        <article>
+            <Image 
+                src={image}
+                height={200}
+                width={200}
+                alt={title+" cover image"}
+            />
+            <h2 className={styles.title}>{title}</h2>
+            <section id={styles.meta_info}>
+                <a href={domain}>{domain}</a>
+                <h3>{date}</h3>
+            </section>
+            <p id={styles.content}>{content}</p>
+            <span className={styles.pill}>{tag}</span>
+        </article>
     )
 }
